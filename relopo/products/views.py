7e09d86c4bb640d17085c21b0123908fed31166e
Andre A <codebyaz@gmail.com>
2023-08-19 19:50:03 +0100

    dev: set allowed method to products

--- relopo/products/views.py
+++ relopo/products/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework import authentication
 
 from .serializers import ProductSerializer
@@ -8,12 +8,16 @@ from .models import Product
 class ProductViewSet(viewsets.ReadOnlyModelViewSet):
     """
     API endpoint that list products.
+
+    * Requires token authentication.
+    * Only admin users are able to access this view.
     """
 
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsAdminUser]
     authentication_classes = [
         authentication.TokenAuthentication, 
         authentication.SessionAuthentication,
     ]
+    http_method_names = ['get']
